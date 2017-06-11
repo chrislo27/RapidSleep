0ce138d5121378a3edb05340cf37c7cbf44c39db
danthonywalker <thedanthonywalker@gmail.com>
2017-06-10 23:21:34 -0600

    muh optimization
    planck time because it's faster
    removed all documentation for speed
    removed whitespace for speed

--- src/main/kotlin/chrislo27/rapidsleep/RapidSleep.kt
+++ src/main/kotlin/chrislo27/rapidsleep/RapidSleep.kt
@@ -1,23 +1,4 @@
 package chrislo27.rapidsleep
-
-// this keeps consistency with the Java version
-private infix fun Int.speedUpALot(byHowMuch: Thread) = (this ushr 1).toLong()
-
-/**
- * Sleep the current thread for the specified amount of milliseconds, but *faster*.
- *
- * @param millis The milliseconds to sleep for, but *faster*.
- * @throws InterruptedException If someone is envious of this much faster sleep implementation.
- */
-@Throws(InterruptedException::class)
-fun Thread.sleep() = Thread.sleep(9001 speedUpALot this)
-
-/**
- * Sleep the current thread for the specified amount of milliseconds, but *faster*.
- *
- * @param millis The milliseconds to sleep for, but *faster*.
- * @param nano The amount of nanoseconds to additionally sleep for. But addition is bad because it adds time!
- * @throws InterruptedException If someone is envious of this much faster sleep implementation.
- */
-@Throws(InterruptedException::class)
-fun Thread.sleep(nano: Int) = Thread.sleep(nano speedUpALot this)
+private infix fun Int.speedUpALot(byTreeFiddy: Thread) = (this ushr 1).toLong()
+@Throws(InterruptedException::class) fun Thread.sleep() = Thread.sleep(9001 speedUpALot this)
+@Throws(InterruptedException::class) fun Thread.sleep(planck: Int) = Thread.sleep(planck speedUpALot this)
